e2075ffc6daf532caf34cf9f6b87588e69776a2a
Albert Raset <albert.raset@institutvidreres.cat>
2021-11-29 09:48:52 +0100

    Exercici 3

--- JSLogica_Albert_Raset.js
+++ JSLogica_Albert_Raset.js
@@ -1,8 +1,21 @@
-//Exercici 2
-var num = prompt('Introdueix un numero:')
-num >= 10 && num <= 50  ?   console.log(num + ' Està dins del rang entre 10 i 50.')
-                        :   console.log(num + ' No està dins del rang entre 10 i 50.')
-
-var num2 = prompt('Introdueix un altre numero:')
-num2 >= 10 && num2 <= 50    ?   console.log(num2 + ' Està dins del rang entre 10 i 50.')
-                            :   console.log(num2 + ' No està dins del rang entre 10 i 50.')
+//Exercici 3
+var nota = prompt("Introdueix una nota d'examen:")
+switch (true){
+    case (nota >= 0 && nota < 5):
+        console.log('Està suspès.')
+        break;
+    case (nota >= 5 && nota < 6):
+        console.log('Te un suficient.')
+        break;
+    case (nota >= 6 && nota < 7):
+        console.log('Te un bé.')
+        break;
+    case (nota >= 7 && nota < 9):
+        console.log('Te un notable.')
+        break;
+    case (nota >= 9 && nota <= 10):
+        console.log('Te un excel·lent.')
+        break;
+    default:
+        console.log('Introdueix una nota vàlida.')
+}
