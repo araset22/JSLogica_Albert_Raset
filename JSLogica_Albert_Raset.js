fd830c1ab4db87cbfb45892808dc36007e44a0d0
Albert Raset <albert.raset@institutvidreres.cat>
2021-11-29 09:08:03 +0100

    Exercici 1

--- JSLogica_Albert_Raset.js
+++ JSLogica_Albert_Raset.js
@@ -1,1 +1,11 @@
-Adria aprova'm
+//Exercici 1
+var num = prompt('Introdueix un numero:')
+if (num % 3 === 0){
+    console.log('És múltiple de 3')
+}
+else if (num % 7 === 0){
+    console.log('És múltiple de 7')
+}
+else {
+    console.log('No és múltiple de 3 ni de 7')
+}
