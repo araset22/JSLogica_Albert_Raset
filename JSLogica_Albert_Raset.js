7bde0f592a7cc73561adb660188c6be0f3107918
Albert Raset <albert.raset@institutvidreres.cat>
2021-11-29 09:28:03 +0100

    Exercici 2

--- JSLogica_Albert_Raset.js
+++ JSLogica_Albert_Raset.js
@@ -1,11 +1,8 @@
-//Exercici 1
+//Exercici 2
 var num = prompt('Introdueix un numero:')
-if (num % 3 === 0){
-    console.log('És múltiple de 3')
-}
-else if (num % 7 === 0){
-    console.log('És múltiple de 7')
-}
-else {
-    console.log('No és múltiple de 3 ni de 7')
-}
+num >= 10 && num <= 50  ?   console.log(num + ' Està dins del rang entre 10 i 50.')
+                        :   console.log(num + ' No està dins del rang entre 10 i 50.')
+
+var num2 = prompt('Introdueix un altre numero:')
+num2 >= 10 && num2 <= 50    ?   console.log(num2 + ' Està dins del rang entre 10 i 50.')
+                            :   console.log(num2 + ' No està dins del rang entre 10 i 50.')
