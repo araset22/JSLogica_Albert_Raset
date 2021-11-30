de21171dddc8badfff8c19d63e241b2c656db476
Albert Raset <albert.raset@institutvidreres.cat>
2021-11-30 08:51:25 +0100

    Exercici 4

--- JSLogica_Albert_Raset.js
+++ JSLogica_Albert_Raset.js
@@ -1,21 +1,9 @@
-//Exercici 3
-var nota = prompt("Introdueix una nota d'examen:")
-switch (true){
-    case (nota >= 0 && nota < 5):
-        console.log('Està suspès.')
-        break;
-    case (nota >= 5 && nota < 6):
-        console.log('Te un suficient.')
-        break;
-    case (nota >= 6 && nota < 7):
-        console.log('Te un bé.')
-        break;
-    case (nota >= 7 && nota < 9):
-        console.log('Te un notable.')
-        break;
-    case (nota >= 9 && nota <= 10):
-        console.log('Te un excel·lent.')
-        break;
-    default:
-        console.log('Introdueix una nota vàlida.')
+//Exercici 4
+var n = 0;
+var num = prompt("Introdueix un número:")
+for(var i = 1; n < num; i++){
+    console.log('La n = ' + n)
+    n+=i
+    
 }
+console.log('El total de n es: ' + n + "\nI el numero d'iteracions són: " + i)
