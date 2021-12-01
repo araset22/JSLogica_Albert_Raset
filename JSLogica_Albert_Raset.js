d1fd36586d2d6cae5c6a7f84df032fff4e5bdd8f
Albert Raset <albert.raset@institutvidreres.cat>
2021-12-01 18:52:09 +0100

    Exercici 7

--- JSLogica_Albert_Raset.js
+++ JSLogica_Albert_Raset.js
@@ -1,29 +1,12 @@
-//Exercici 6
+//Exercici 7
 
-var num1  = prompt('Introdueix un número:')
-var num2  = prompt('Introdueix un segon número:')
-var num3  = prompt('Introdueix un tercer número:')
+var num = prompt('Introdueix un número:')
 
-function ordenar(num1,num2,num3){
-
-    if (num1>=num2 && num2>=num3) {
-        console.log(num1 + ' ' + num2 + ' ' + num3)
-    }
-    else if (num1>=num3 && num3>=num2) {
-        console.log(num1 + ' ' + num3 + ' ' + num2)
-    }
-    else if(num2>=num1 && num1>=num3){
-        console.log(num2 + ' ' + num1 + ' ' + num3)
-    }
-    else if(num2>=num3 && num3>=num1){
-        console.log(num2 + ' ' + num3 + ' ' + num1)
-    }
-    else if (num3>=num1 && num1>=num2) {
-        console.log(num3 + ' ' + num1 + ' ' + num2)
+for(var i = 0; i <= num; i++){
+    if (i %2 === 0) {
+        console.log(i + ' es un numero parell.')
     }
     else{
-        console.log(num3 + ' ' + num2 + ' ' + num1)
+        console.log(i + ' es un numero senar.')
     }
-
 }
-console.log(ordenar(num1,num2,num3))
