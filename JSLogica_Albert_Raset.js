aa41f8b25e575fa11b30e1a0dabd3cb34a1ca367
Albert Raset <albert.raset@institutvidreres.cat>
2021-12-01 18:32:41 +0100

    Exercici 6

--- JSLogica_Albert_Raset.js
+++ JSLogica_Albert_Raset.js
@@ -1,11 +1,29 @@
-//Exercici 5
+//Exercici 6
 
-var text  = "1g22 1h4te 2ary 54st2 5yu4s"
-var num = text.replace(/[^0-9]/gi, "")
-var suma = 0
+var num1  = prompt('Introdueix un número:')
+var num2  = prompt('Introdueix un segon número:')
+var num3  = prompt('Introdueix un tercer número:')
 
-for (var i = 0; i < num.length; i++){
-    suma += Number(num.charAt(i))
+function ordenar(num1,num2,num3){
+
+    if (num1>=num2 && num2>=num3) {
+        console.log(num1 + ' ' + num2 + ' ' + num3)
+    }
+    else if (num1>=num3 && num3>=num2) {
+        console.log(num1 + ' ' + num3 + ' ' + num2)
+    }
+    else if(num2>=num1 && num1>=num3){
+        console.log(num2 + ' ' + num1 + ' ' + num3)
+    }
+    else if(num2>=num3 && num3>=num1){
+        console.log(num2 + ' ' + num3 + ' ' + num1)
+    }
+    else if (num3>=num1 && num1>=num2) {
+        console.log(num3 + ' ' + num1 + ' ' + num2)
+    }
+    else{
+        console.log(num3 + ' ' + num2 + ' ' + num1)
+    }
+
 }
-
-console.log(suma);
+console.log(ordenar(num1,num2,num3))
